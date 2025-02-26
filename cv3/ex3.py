d37f106022c20806fbfb3135b889d3e85b40b219
Katzoun <tomas.janousek02@gmail.com>
2025-02-26 23:44:52 +0100

    Implement Hooke-Jeeves method but not working

--- cv3/ex3.py
+++ cv3/ex3.py
@@ -37,6 +37,8 @@ def cyclic_coordinate_search_accelerated(startpoint, step_size_lim, max_iter, ma
 
     points = np.array([startpoint], dtype=np.float64)
 
+    #todo implementace limitu na minimalni velikost delta
+
     while np.fabs(delta) > step_size_lim and it_num < max_iter and f_calls < max_fcalls:
         x1 = x
         for i in range(n):
@@ -60,12 +62,34 @@ def hookes_jeeves(startpoint, start_step_size, step_size_lim, step_decay, max_it
     n = np.size(x)
     it_num = 0
     f_calls = 0
-
     points = np.array([startpoint], dtype=np.float64)
 
+    y = rosenbrock(x[0], x[1])
+    f_calls += 1
+
+    while start_step_size > step_size_lim and it_num < max_iter and f_calls < max_fcalls:
+        improved = False
+        xbest, ybest = x, y
+
+        for i in range(1,n):
+            for sgn in (-1,1):
+                xnew = x + sgn*start_step_size*np.eye(n)[i]
+                ynew = rosenbrock(xnew[0], xnew[1])
+                f_calls += 1
+
+                if ynew < ybest:
+                    xbest, ybest = xnew, ynew
+                    improved = True
+                
+        x,y = xbest, ybest
+        points = np.append(points, [x], axis=0)
+
+        if not improved:
+            start_step_size *= step_decay
+        
+        it_num += 1
+
     return points, f_calls, it_num
-
-
 
 
 def line_search(startpoint,direction, max_fcalls):
@@ -76,47 +100,15 @@ def line_search(startpoint,direction, max_fcalls):
     return newpoint, calls
 
 
-# def gradient_descent(startpoint, grad_size_lim, max_iter, search_method):
-#     x = startpoint.astype(float)
-    
-#     points = np.empty((0, 2))
-#     directions = np.empty((0, 2))
-#     points = np.append(points, [startpoint], axis=0)
-
-#     for i in range(max_iter):
-#         grad = rosenbrock_grad(x[0], x[1])
-#         if np.linalg.norm(grad) < grad_size_lim:
-#             if search_method == "optimal":
-#                 print(f"Gradient descent with optimal step converged (grad < {grad_size_lim}) at iteration {i}, distance to global minimum: {np.linalg.norm(x-[1,1])}")
-#             elif search_method == "decaying":
-#                 print(f"Gradient descent with decaying step converged (grad < {grad_size_lim}) at iteration {i}, distance to global minimum: {np.linalg.norm(x-[1,1])}")
-#             break
-        
-#         elif i == max_iter-1:
-#             if search_method == "optimal":
-#                 print(f"Gradient descent with optimal step did not converge after {max_iter} iterations, distance to global minimum: {np.linalg.norm(x-[1,1])}")
-#             elif search_method == "decaying":
-#                 print(f"Gradient descent with decaying step did not converge after {max_iter} iterations, distance to global minimum: {np.linalg.norm(x-[1,1])}")
-
-#         dir = direction(grad)
-
-#         if search_method == "optimal":
-#             x[0],x[1] = line_search([x[0],x[1]], dir)
-#         elif search_method == "decaying":
-#             x[0], x[1] = line_search_conjugate([x[0],x[1]], dir,i)
-#         else:
-#             print("Invalid search method")
-#             raise ValueError
-        
-#         points = np.append(points, [x], axis=0)
-#         directions = np.append(directions, [dir], axis=0)
-
-#     return points, directions
 
 if __name__ == '__main__':
 
-    step_size_lim = 0.0001 
-    start_step_size = 1 # for Hooke Jeeves
+    step_size_lim_ccs = 0.0001      # for cyclic coordinate search
+
+    step_size_lim_hj = 0.000001     # for Hooke Jeeves
+    start_step_size_hj = 1          # for Hooke Jeeves
+    step_decay_hj  = 0.5            # for Hooke Jeeves
+
     max_iter = 100
     max_fcalls = 1000
     startpoint = np.array([-1,-1])
@@ -134,17 +126,16 @@ if __name__ == '__main__':
 
     print(f"\n\nGlobal minimum at (1,1) with value {rosenbrock(1,1)}")
     print(f"Starting point: {startpoint}")
-    print(f"Step size limit: {step_size_lim}")
     print(f"Max iterations: {max_iter}")
     print(f"Max function calls: {max_iter}")
 
     print("\nCyclic coordinate search with acceleration step and optimal stepsize: ")
-    points_ccs, f_calls_ccs, it_num_ccs = cyclic_coordinate_search_accelerated(startpoint, step_size_lim, max_iter, max_fcalls)
+    points_ccs, f_calls_ccs, it_num_ccs = cyclic_coordinate_search_accelerated(startpoint, step_size_lim_ccs, max_iter, max_fcalls)
     print(f"Converged after {it_num_ccs} iterations and {f_calls_ccs} function calls, distance to global minimum: {np.linalg.norm(points_ccs[-1]-[1,1])}")
 
     print("\nHooke Jeeves method: ")
-    points_hj, f_calls_hj, it_num_hj  = hookes_jeeves(startpoint, start_step_size, step_size_lim, max_iter, "decaying")
-    print(f"Converged after {it_num_ccs} iterations and {f_calls_ccs} function calls, distance to global minimum: {np.linalg.norm(points_ccs[-1]-[1,1])}")
+    points_hj, f_calls_hj, it_num_hj  = hookes_jeeves(startpoint, start_step_size_hj, step_size_lim_hj,step_decay_hj, max_iter, max_fcalls)
+    print(f"Converged after {it_num_hj} iterations and {f_calls_hj} function calls, distance to global minimum: {np.linalg.norm(points_hj[-1]-[1,1])}")
 
     # print("\nConjugate gradient method with Polak-Ribiere equation and optimal stepsize: ")
     # points_cg, directions_cg = gradient_descent_conjugate(startpoint, grad_size_lim, max_iter)
@@ -166,8 +157,7 @@ if __name__ == '__main__':
     
     #3D plot cesty
     ax.scatter(points_ccs[1:,0], points_ccs[1:,1], rosenbrock(points_ccs[1:,0], points_ccs[1:,1]), c='b', marker='o')
-    #gradient descent decaying
-    #ax.scatter(points_gd_dec[1:,0], points_gd_dec[1:,1], rosenbrock(points_gd_dec[1:,0], points_gd_dec[1:,1]), c='y', marker='o')
+    ax.scatter(points_hj[1:,0], points_hj[1:,1], rosenbrock(points_hj[1:,0], points_hj[1:,1]), c='y', marker='o')
     #conjugate descent
     #ax.scatter(points_cg[1:,0], points_cg[1:,1], rosenbrock(points_cg[1:,0], points_cg[1:,1]), c='orange', marker='o')
     ax.set_xlabel('x1')
@@ -183,7 +173,7 @@ if __name__ == '__main__':
 
     #2D plot cesty
     ax.scatter(points_ccs[1:,0], points_ccs[1:,1], c='b', marker='o')
-    #ax.scatter(points_gd_dec[1:,0], points_gd_dec[1:,1], c='y', marker='o')
+    ax.scatter(points_hj[1:,0], points_hj[1:,1], c='y', marker='o')
     #ax.scatter(points_cg[1:,0], points_cg[1:,1], c='orange', marker='o')
 
 
